4e8234ee83ad9d178577465b35749eff3977229f
SaLemandr <alexlepsh@tut.by>
2020-02-27 18:09:48 +0300

    bot

---
+++ bot_discord.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from time import *
+
+Part_bilet = 'NjgyMjQyNDI4MTkwNTg5MDI2.XlaLYQ.E9B806UKTYP5mDdG6RxFxl1KyNc'
+bot = commands.Bot(command_prefix = '!')
+
+
+emojis = {
+'wtf' : '<:emoji_9:681485164609929218>',
+'fuck_you' : '<:emoji_8:681481825272725562>',
+'no_fuck_you' : '<:emoji_7:681476030258741289>',
+'mmm' : '<:emoji_6:675406943103483904>',
+'mmm_full' : '<:emoji_5:675388729661325332>',
+'peka' : '<:peka:681832106489675823>',
+'pled' : '<:Pled:674196648519532574>',
+'cool_peka' : '<:Pled:674196648519532574>',
+'peka_chudo' : '<:peka_chudo:674205087337414676>'
+}
+
+
+@bot.command(pass_context=True) #разрешаем передавать агрументы
+
+async def emoji(ctx, arg):
+    await ctx.send(emojis[arg])
+
+async def test(ctx, arg): #создаем асинхронную фунцию бота
+    await ctx.send(arg) #отправляем обратно аргумент
+    print(arg)
+
+bot.run(Part_bilet)
